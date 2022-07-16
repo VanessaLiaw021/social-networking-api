91831144b0c87c6f815d63fb2c9ea9591cb97c08
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-15 21:44:51 -0400

    added deleteUser and addfriend routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,5 @@
 //Import model 
-const User = require("../models/User");
+const { User, Thought } = require("../models");
 
 //Export the following functions 
 module.exports = {
@@ -7,28 +7,45 @@ module.exports = {
     //Function that get all users 
     getAllUsers(req, res) {
         User.find()
+            .populate({ path: "thoughts", select: "-__v "})
+            .select("-__v")
             .then(allUser => res.json(allUser))
-            .catch(err => res.status(500).json(err));
+            .catch(err => res.status(500).json(err))
     },
 
     //Function that get a single user 
     getSingleUser(req, res) {
-        User.findOne({ id: req.params.id })
-            .then(singleUser => res.json(singleUser))
+        User.findOne({ _id: req.params.id })
+            .populate({ path: "friends", select: "-__v" })
+            .populate({ path: "thoughts", select: "-__v" })
+            .select("-__v")
+            .then(user => res.json(user))
             .catch(err => res.status(500).json(err))
     },
 
     //Function that add a new user 
     createUser(req, res) {
-        User.create(req.body)
-            .then(addUser => res.json(addUser))
-            .catch(err => res.status(500).json(err))
+        User.create(req.body).then(addUser => res.json(addUser)).catch(err => res.status(500).json(err))
     },
 
     //Function that update a user 
     updateUser(req, res) {
-        User.findOneAndUpdate({ id: req.params.id }, req.body, { new: true, runValidators: true })
+        User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true })
             .then(update => res.json(update))
             .catch(err => res.status(500).json(err))
-    }
+    },
+    
+    //Function that delete a user 
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.id })
+            .then(delete => res.json(delete))
+            .catch(err => res.status(500).json(err))
+    },
+
+    //Function that add a friend 
+    addFriend(req, res) {
+        User.findOneAndUpdate({ _id: req.params.id }, { $push: { friends: req.params.friendId }}, { new: true })
+            .then(friend => res.json(friend))
+            .catch(err => res.status(500).json(err))
+    },
 };
