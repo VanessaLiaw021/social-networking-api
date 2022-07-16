cfc0633b9cba1d67bc0d15953235046fdaca9945
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-15 21:17:18 -0400

    created routes to get a single users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,7 +7,14 @@ module.exports = {
     //Function that get all users 
     getAllUsers(req, res) {
         User.find()
-        .then(user => res.json(user))
-        .catch(err => res.status(500).json(err));
+            .then(allUser => res.json(allUser))
+            .catch(err => res.status(500).json(err));
+    },
+
+    //Function that get a single user 
+    getSingleUser(req, res) {
+        User.findOne({ id: req.params.id })
+            .then(singleUser => res.json(singleUser))
+            .catch(err => res.status(500).json(err))
     },
 };
